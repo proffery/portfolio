1bdf8d31d9cec5962b9f23000938f3bc1ced0365
Dmitry Shamko <proffery@gmail.com>
2023-09-22 19:45:21 +0300

    my projects data object

---
+++ src/data/projects.tsx
@@ -0,0 +1,148 @@
+
+import Recipes from '../assets/images/projects/MyFirstWebpage.webp'
+import LandingPage from '../assets/images/projects/LandingPage.webp'
+import SignUpForm from '../assets/images/projects/SignUpForm.webp'
+import AdminDashboard from '../assets/images/projects/AdminDashboard.webp'
+import RockPaperScissors from '../assets/images/projects/RockPaperScissors.webp'
+import Calculator from '../assets/images/projects/Calculator.webp'
+import EtchASketch from '../assets/images/projects/EtchASketch.webp'
+import Library from '../assets/images/projects/Library.webp'
+import TicTacToe from '../assets/images/projects/TicTacToe.webp'
+import TodoList from '../assets/images/projects/TodoList.webp'
+import WeatherApp from '../assets/images/projects/WeatherApp.webp'
+import CvApplication from '../assets/images/projects/CvApplication.webp'
+import MemoryCard from '../assets/images/projects/MemoryCard.webp'
+import ShoppingCart from '../assets/images/projects/ShoppingCart.webp'
+import BlogApp from '../assets/images/projects/BlogApp.webp'
+import PortfolioProject from '../assets/images/projects/PortfolioProject.webp'
+import PhotographerHomePage from '../assets/images/projects/PhotographerHomePage.webp'
+
+
+export const projects = [
+    {
+        imageUrl: Recipes,
+        projectDemo: 'https://proffery.github.io/odin-recipes/',
+        projectCode: 'https://github.com/proffery/odin-recipes',
+        projectTitle: 'My First Webpage. Project: Recipes',
+        projectCategory: 'HTML, CSS'
+    },
+    {
+        imageUrl: LandingPage,
+        projectDemo: 'https://proffery.github.io/odin-landing-page/',
+        projectCode: 'https://github.com/proffery/odin-landing-page',
+        projectTitle: 'Project: Landing Page. Foundations Course',
+        projectCategory: 'HTML, CSS'
+    },
+    {
+        imageUrl: SignUpForm,
+        projectDemo: 'https://proffery.github.io/odin-sign-up-form/',
+        projectCode: 'https://github.com/proffery/odin-sign-up-form',
+        projectTitle: 'Project: Sign-Up Form. Foundations Course',
+        projectCategory: 'HTML, CSS'
+    },
+    {
+        imageUrl: AdminDashboard,
+        projectDemo: 'https://proffery.github.io/odin-admin-dashboard/',
+        projectCode: 'https://github.com/proffery/odin-admin-dashboard',
+        projectTitle: 'Project: Admin Dashboard. Foundations Course',
+        projectCategory: 'HTML, CSS'
+    },
+    {
+        imageUrl: AdminDashboard,
+        projectDemo: 'https://proffery.github.io/odin-rock-paper-scissors/',
+        projectCode: 'https://github.com/proffery/odin-rock-paper-scissors',
+        projectTitle: 'Project: Rock Paper Scissors',
+        projectCategory: 'Java Script'
+    },
+    {
+        imageUrl: RockPaperScissors,
+        projectDemo: 'https://proffery.github.io/odin-admin-dashboard/',
+        projectCode: 'https://github.com/proffery/odin-admin-dashboard',
+        projectTitle: 'Project: Rock Paper Scissors',
+        projectCategory: 'Java Script'
+    },
+    {
+        imageUrl: Calculator,
+        projectDemo: 'https://proffery.github.io/odin-calculator/',
+        projectCode: 'https://github.com/proffery/odin-calculator',
+        projectTitle: 'Project: Calculator',
+        projectCategory: 'Java Script'
+    },
+    {
+        imageUrl: EtchASketch,
+        projectDemo: 'https://proffery.github.io/odin-etch-a-sketch/',
+        projectCode: 'https://github.com/proffery/odin-etch-a-sketch',
+        projectTitle: 'Project: Etch-a-Sketch',
+        projectCategory: 'Java Script'
+    },
+    {
+        imageUrl: Library,
+        projectDemo: 'https://proffery.github.io/odin-library/',
+        projectCode: 'https://github.com/proffery/odin-library',
+        projectTitle: 'Project: Library',
+        projectCategory: 'Java Script'
+    },
+    {
+        imageUrl: TicTacToe,
+        projectDemo: 'https://proffery.github.io/odin-tic-tac-toe/',
+        projectCode: 'https://github.com/proffery/odin-tic-tac-toe',
+        projectTitle: 'Project: Tic Tac Toe',
+        projectCategory: 'Java Script'
+    },
+    {
+        imageUrl: TodoList,
+        projectDemo: 'https://proffery.github.io/odin-todo-list/',
+        projectCode: 'https://github.com/proffery/odin-todo-list',
+        projectTitle: 'Project: Todo List',
+        projectCategory: 'Java Script'
+    },
+    {
+        imageUrl: WeatherApp,
+        projectDemo: 'https://proffery.github.io/odin-weather-app/',
+        projectCode: 'https://github.com/proffery/odin-weather-app',
+        projectTitle: 'Project: Weather App',
+        projectCategory: 'Java Script'
+    },
+    {
+        imageUrl: CvApplication,
+        projectDemo: 'https://proffery.github.io/odin-cv-application/',
+        projectCode: 'https://github.com/proffery/odin-cv-application',
+        projectTitle: 'Project: CV Application',
+        projectCategory: 'React'
+    },
+    {
+        imageUrl: MemoryCard,
+        projectDemo: 'https://proffery.github.io/odin-memory-card/',
+        projectCode: 'https://github.com/proffery/odin-memory-card',
+        projectTitle: 'Project: Memory Card',
+        projectCategory: 'React'
+    },
+    {
+        imageUrl: ShoppingCart,
+        projectDemo: 'https://proffery.github.io/odin-shopping-cart/',
+        projectCode: 'https://github.com/proffery/odin-shopping-cart',
+        projectTitle: 'Project: Shopping Cart',
+        projectCategory: 'React'
+    },
+    {
+        imageUrl: BlogApp,
+        projectDemo: 'https://proffery.github.io/blog-app/',
+        projectCode: 'https://github.com/proffery/blog-app',
+        projectTitle: 'Simple Blog App',
+        projectCategory: 'React'
+    },
+    {
+        imageUrl: PortfolioProject,
+        projectDemo: 'https://proffery.github.io/portfolio-project-one/',
+        projectCode: 'https://github.com/proffery/portfolio-project-one',
+        projectTitle: 'Animated Portfolio Page',
+        projectCategory: 'React'
+    },
+    {
+        imageUrl: PhotographerHomePage,
+        projectDemo: 'https://proffery.github.io/portfolio-project-one/',
+        projectCode: 'https://github.com/proffery/portfolio-project-two',
+        projectTitle: 'Responsive Photographer Website React-Bootstrap',
+        projectCategory: 'React'
+    },
+]
