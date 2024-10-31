30ff3a857bbb56c661827de0b5c640bada618980
Dmitry Shamko <proffery@gmail.com>
2024-10-31 16:48:07 +0300

    opengraph image

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -19,20 +19,18 @@ export async function generateMetadata({ params: { locale } }: Props) {
   const { metadata } = dict
 
   return {
-    alternates: { canonical: `/${locale}/` },
+    alternates: { canonical: `/` },
     applicationName: metadata.applicationName,
     description: metadata.description,
     keywords: metadata.description.replaceAll(' ', ','),
-    metadataBase: new URL(
-      process.env.NEXT_PUBLIC_HOST_BASE + '/' + locale ?? `http://localhost:3000`
-    ),
+    metadataBase: new URL(process.env.NEXT_PUBLIC_HOST_BASE ?? `http://localhost:3000`),
     openGraph: {
       description: metadata.description,
       locale,
       siteName: metadata.applicationName,
       title: metadata.title,
       type: 'website',
-      url: process.env.NEXT_PUBLIC_HOST_BASE + '/' + locale,
+      url: process.env.NEXT_PUBLIC_HOST_BASE,
     },
     title: { default: metadata.title, template: `%s | ${metadata.title}` },
     twitter: {
