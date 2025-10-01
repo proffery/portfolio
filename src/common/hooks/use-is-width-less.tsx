e06f5ed04d78400815cc00dc70d804207046fe95
Dmitry Shamko <dmitry.shamko@celadonsoft.com>
2025-10-01 10:56:25 +0300

    refactoring

--- src/common/hooks/use-is-width-less.tsx
+++ src/common/hooks/use-is-width-less.tsx
@@ -1,40 +1,17 @@
-// 'use client'
-// import { useEffect, useState } from 'react'
-//
-// export const useIsWidthLess = (width: number) => {
-//   const [currentWidth, setCurrentWidth] = useState<number>(width)
-//   const handleWindowResize = () => setCurrentWidth(window.innerWidth)
-//
-//   useEffect(() => {
-//     handleWindowResize()
-//
-//     window.addEventListener('resize', handleWindowResize)
-//
-//     return () => window.removeEventListener('resize', handleWindowResize)
-//   }, [width, currentWidth])
-//
-//   return currentWidth < width
-// }
 'use client'
 import { useEffect, useState } from 'react'
 
 export const useIsWidthLess = (width: number) => {
   const [currentWidth, setCurrentWidth] = useState<number>(width)
+  const handleWindowResize = () => setCurrentWidth(window.innerWidth)
 
   useEffect(() => {
-    const handleWindowResize = () => {
-      const newWidth = window.innerWidth
-
-      if (newWidth !== currentWidth) {
-        setCurrentWidth(newWidth)
-        window.location.reload()
-      }
-    }
+    handleWindowResize()
 
     window.addEventListener('resize', handleWindowResize)
 
     return () => window.removeEventListener('resize', handleWindowResize)
-  }, [currentWidth, width])
+  }, [width, currentWidth])
 
   return currentWidth < width
 }
